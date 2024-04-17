ac23efb9084aab1549116a92e900aa9d0f91b7fd
Tsanfer <a1124851454@gmail.com>
2024-04-17 08:45:38 +0800

    Update test_group_competition.py

--- software-testing/往届真题/第十五届/模拟赛第一期/题目/单元测试 Python 源码包/test_group_competition.py
+++ software-testing/往届真题/第十五届/模拟赛第一期/题目/单元测试 Python 源码包/test_group_competition.py
@@ -9,6 +9,23 @@ from cn.lanqiao.group_competition import GroupCompetition
 class GroupCompetitionTest(unittest.TestCase):
     def test_grouping_01(self):
         # TODO 请填写单元测试代码
+        self.assertEqual(GroupCompetition.grouping(20, 70), "无复赛资格")
+
+    def test_grouping_02(self):
+        # TODO 请填写单元测试代码
+        self.assertEqual(GroupCompetition.grouping(10, 70), "少儿B组")
+
+    def test_grouping_03(self):
+        # TODO 请填写单元测试代码
+        self.assertEqual(GroupCompetition.grouping(10, 80), "少儿A组")
+
+    def test_grouping_04(self):
+        # TODO 请填写单元测试代码
+        self.assertEqual(GroupCompetition.grouping(16, 70), "青少年B组")
+
+    def test_grouping_05(self):
+        # TODO 请填写单元测试代码
+        self.assertEqual(GroupCompetition.grouping(16, 80), "青少年A组")
 
 
 if __name__ == '__main__':
